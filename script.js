627a53edac073e9d5d5853cc2c97e594d7dfe5ea
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-09-30 23:01:06 -0700

    hours and input text

---
+++ script.js
@@ -0,0 +1,32 @@
+var time = moment().format("hh:mm:ss");
+$("#time").text(time);
+
+var today = moment();
+$("#day").text(today.format("MMM Do, YYYY"));
+
+var sheduleFormEl = $("#shedule-form");
+var toDoListEl = $("#to-do-list");
+var saveEl = $("#icon")
+
+// create function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  // select form element by its `name` attribute and get its value
+  var activity = $('input[name="list-input"]').val();
+
+  // if there's nothing in the form entered, don't print to the page
+  if (!activity) {
+    console.log('No things to do at this moment!');
+    return;
+  }
+
+  // print to the page
+  toDoListEl.append('<li>' + activity + '</li>');
+
+  // clear the form input element
+  $('input[name="list-input"]').val('');
+}
+
+// Create a submit event listener on the form element
+saveEl.on('submit', handleFormSubmit);
