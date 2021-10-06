4a9d3090760dbdd8d773cbf8e45728eeb35505e6
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-10-05 18:54:12 -0700

    re organization and local storage

--- script.js
+++ script.js
@@ -7,34 +7,27 @@ $("#day").text(today.format("MMM Do, YYYY"));
 var sheduleFormEl = $("#shedule-form");
 var toDoListEl = $("#to-do-list");
 var saveEl = $("#icon")
+var timeBlock = $(".time-block")
 
 // create function to handle form submission
 function handleFormSubmit(event) {
   event.preventDefault();
-
+  console.log($(this).prev().val());
   // select form element by its `name` attribute and get its value
-  var activity = $('input[name="list-input"]').val();
-
+  var activity = $(this).prev().val();
+  var id = + $(this).prev().attr("id")
   // if there's nothing in the form entered, don't print to the page
   if (!activity) {
     console.log('No things to do at this moment!');
     return;
   };
 
-  // print to the page
-  toDoListEl.append('<li>' + activity + '</li>');
-  
-
-  // clear the form input element
-  $('input[name="list-input"]').val('');
-
-  
-      localStorage.setItem('activity', JSON.stringify(avtivity));
-      localStorage.getItem("activity")
-  };
+      localStorage.setItem(id, JSON.stringify(activity));
+     // localStorage.getItem("activity")
+};
   
   
 
 
 // Create a submit event listener on the form element
-saveEl.on('click', handleFormSubmit);
+timeBlock.on('click', ".saveBtn", handleFormSubmit);
